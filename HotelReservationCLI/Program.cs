dc13bf9edf3c0c19e7673ed31ae85ec004f547f0
Edgar Yépez <edgaryepezec@gmail.com>
2019-01-16 19:58:08 -0500

    CLI project.

---
+++ HotelReservationCLI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HotelService;
+
+namespace HotelReservationCLI {
+
+	class Program {
+
+		static void Main(string[] args) {
+			string HotelDirectoryFile = @"D:\Projects\C#\HotelReservation_CSharp\HotelReservationCLI\MiamiHotels.txt";
+			string InputFile = @"D:\Projects\C#\HotelReservation_CSharp\HotelReservationCLI\Input.txt";
+
+			HotelDirectory Directory = HotelDirectory.Factory.CreateFromFile(HotelDirectoryFile);
+
+			foreach (Input I in Input.Factory.CreateFromFile(InputFile)) {
+				HotelInvoice CheapestInvoice = Directory.FindCheapestInvoice(I.Client, I.ReservationDates);
+				Console.WriteLine($"{CheapestInvoice.Hotel.Name}");
+			}
+			Console.ReadKey();
+		}
+
+	}
+
+}
